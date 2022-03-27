71340a27a97d6c1bd413299a1cfe4c3ee9e08c32
Marcus Lilja <90m.hansson@gmail.com>
2022-03-27 13:14:15 +0200

    Layers now use Numpy

--- layer.py
+++ layer.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 inputs = [1, 2, 3, 2.5]
 weights =  [[0.2, 0.8, -0.5, 1.0],
             [0.5, -0.91, 0.26, -0.5],
@@ -5,24 +7,8 @@ weights =  [[0.2, 0.8, -0.5, 1.0],
 
 biases = [2, 3, 0.5]
 
-# Output for the current layer
-layer_outputs = []
-
-# For each neuron
-for neuron_weights, neuron_bias in zip(weights, biases):
-    # Zeroed output of given neuron
-    neuron_output = 0
-
-    # For each input and weight to the neuron
-    for n_input, weight in zip(inputs, neuron_weights):
-        # Multiply this inout by its associated weight
-        # and add the neuron's output variable
-        neuron_output += n_input*weight
-    
-    # Add the bias
-    neuron_output += neuron_bias
-    # Out neuron's result to the layer's output list
-    layer_outputs.append(neuron_output)
+#  Add biases to the dot products
+layer_outputs = np.dot(weights, inputs) + biases
 
 
 print(layer_outputs)
