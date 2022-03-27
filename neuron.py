079939c246dbc831e82f82b3b2477a22b38f3d74
Marcus Lilja <90m.hansson@gmail.com>
2022-03-27 13:07:24 +0200

    Neuron now use Numpy

--- neuron.py
+++ neuron.py
@@ -1,10 +1,10 @@
-inputs = [1, 2, 3, 2.5]
-weights = [0.2, 0.8, -0.5, 1.0]
-bias = 2
+import numpy as np
 
-output = (  inputs[0]*weights[0] +
-            inputs[1]*weights[1] +
-            inputs[2]*weights[2] + 
-            inputs[3]*weights[3] +bias)
+# Set test inputs weights and bias.
+inputs = [1.0, 2.0, 3.0, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2.0
+
+output = np.dot(weights, inputs) + bias
 
 print(output)
