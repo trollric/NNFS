02e7b8a6d8f1ec3eafe04b61909813e45081b4f6
Marcus Lilja <90m.hansson@gmail.com>
2022-03-28 13:11:47 +0200

    Dense layer init sets weighs and biases

--- dense_layer.py
+++ dense_layer.py
@@ -1,13 +1,13 @@
 # Imports
-
+import numpy as np
 
 # Dense layer
 class Layer_Dense:
 
     # Layer initialization
     def __init__(self, n_inputs, n_neurons):
-        # Initialize weights and biases
-        pass
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
     # Forward pass
     def forward(self, inputs):
