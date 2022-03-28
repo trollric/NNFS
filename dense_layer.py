58442c6e4a6b08028243afc2e80e611de2eb554e
Marcus Lilja <90m.hansson@gmail.com>
2022-03-28 13:06:31 +0200

    set up dense layer class in separate script

---
+++ dense_layer.py
@@ -0,0 +1,15 @@
+# Imports
+
+
+# Dense layer
+class Layer_Dense:
+
+    # Layer initialization
+    def __init__(self, n_inputs, n_neurons):
+        # Initialize weights and biases
+        pass
+
+    # Forward pass
+    def forward(self, inputs):
+        # Calculate output values from inputs, weights and biases
+        pass
