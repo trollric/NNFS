3d966e146b080b569d788dde7c1ba6f90e1c6944
Marcus Lilja <90m.hansson@gmail.com>
2022-05-03 13:59:47 +0200

    Added normalization to test

--- softmax_test.py
+++ softmax_test.py
@@ -1,4 +1,5 @@
 layer_outputs = [4.8, 1.21, 2.385]
+from cmath import exp
 import math
 
 from numpy import append
@@ -10,7 +11,18 @@ E = math.e
 exp_values = []
 
 for output in layer_outputs:
-    exp_values,append(E**output)
+    exp_values.append(E**output)
 
 print('Exponentiated values:')
 print(exp_values)
+
+# Normalize the values
+norm_base = sum(exp_values)
+norm_values = []
+
+for value in exp_values:
+    norm_values.append(value/norm_base)
+
+print('Normalized exponentiated values:')
+print(norm_values)
+print('Sum of normalized values: {}'.format(sum(norm_values)))
