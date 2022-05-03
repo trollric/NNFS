cd7467503dc2b2a46dbf124be9fc1b53fecacd66
Marcus Lilja <90m.hansson@gmail.com>
2022-05-03 14:04:53 +0200

    Test now use numpy

--- softmax_test.py
+++ softmax_test.py
@@ -1,28 +1,13 @@
 layer_outputs = [4.8, 1.21, 2.385]
-from cmath import exp
-import math
+import numpy as np
 
-from numpy import append
-
-# Exponentiate the numbers Y = e^x
-E = math.e
-
-# For each value in a vector, calculate the exponentiated value
-exp_values = []
-
-for output in layer_outputs:
-    exp_values.append(E**output)
-
-print('Exponentiated values:')
+# With numpy
+# Exoinentiate the numbers
+exp_values = np.exp(layer_outputs)
 print(exp_values)
 
 # Normalize the values
-norm_base = sum(exp_values)
-norm_values = []
-
-for value in exp_values:
-    norm_values.append(value/norm_base)
-
+norm_values = exp_values / np.sum(exp_values)
 print('Normalized exponentiated values:')
 print(norm_values)
 print('Sum of normalized values: {}'.format(sum(norm_values)))
