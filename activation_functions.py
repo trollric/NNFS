c5f2d13a6e03ba1e0f1cdfd995a8b27da06e0255
Marcus Lilja <90m.hansson@gmail.com>
2022-05-03 14:25:04 +0200

    added softmax activation class

--- activation_functions.py
+++ activation_functions.py
@@ -1,3 +1,4 @@
+from turtle import forward
 import numpy as np
 
 class Activation_ReLU:
@@ -13,3 +14,21 @@ class Activation_ReLU:
         """
         # Calculate output values from input
         self.output = np.maximum(0, inputs)
+
+class Activation_Softmax:
+    """Softmax activation. Exponentates and normalizes the values inputed
+    """
+    # Forward pass
+    def forward(self, inputs):
+        """Takes a batch of inputs and outputs the normalized exponential as probablilities
+
+        Args:
+            inputs (np.array): Batch of input values
+        """
+        # Get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+
+        # Normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = probabilities
