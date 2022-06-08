8c962bb82b810df2ba57bfb7412a57c045f73fd3
Marcus Lilja <90m.hansson@gmail.com>
2022-06-08 12:14:35 +0200

    index test with np array

--- cross_entropy_test.py
+++ cross_entropy_test.py
@@ -27,3 +27,13 @@ class_targets =[0, 1, 1]
 
 for target_index, distribution in zip(class_targets, softmax_output):
     print(distribution[target_index])
+
+print('Test with numpy array')
+
+softmax_output = np.array([  [0.7, 0.1, 0.2],
+                    [0.1, 0.5, 0.4],
+                    [0.02, 0.9, 0.08]])
+
+class_targets =[0, 1, 1]
+
+print(softmax_output[[0, 1, 2], class_targets])
