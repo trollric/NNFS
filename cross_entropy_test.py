69c9734f87b4fded3e04617f0ed88011ad0e6b75
Marcus Lilja <90m.hansson@gmail.com>
2022-06-08 12:31:29 +0200

    handles sparse/hot ecnoded

--- cross_entropy_test.py
+++ cross_entropy_test.py
@@ -54,3 +54,32 @@ neg_log = -np.log(softmax_output[
 
 average_loss = np.mean(neg_log)
 print(average_loss)
+
+
+print('now with a check if targets are one hot encoded or sparse')
+
+softmax_output = np.array([  [0.7, 0.1, 0.2],
+                    [0.1, 0.5, 0.4],
+                    [0.02, 0.9, 0.08]])
+
+class_targets =np.array([[1, 0, 0],
+                        [0, 1, 0],
+                        [0, 1, 0]])
+
+# Probablilities for target values
+# only if categorical labels
+if len(class_targets.shape) == 1:
+    correct_confidences = softmax_output[
+        range(len(softmax_output)), class_targets
+    ]
+elif len(class_targets.shape) == 2:
+    correct_confidences = np.sum(
+        softmax_output * class_targets,
+        axis=1
+    )
+
+# Get the negative ln
+neg_log = -np.log(correct_confidences)
+
+average_loss = np.mean(neg_log)
+print(average_loss)
